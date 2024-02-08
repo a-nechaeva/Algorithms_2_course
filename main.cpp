2556efcf113823ebd43007be0c28609b65d279cc
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-02-08 16:30:19 +0300

    1005 и 2025 готовы к защите

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main() {
+    int numb_test;
+    std::cin >> numb_test;
+
+    for (int i = 0; i < numb_test; ++i) {
+
+        int fighters, teams;
+        std::cin >> fighters;
+        std::cin >> teams;
+
+        int medium_in_team = fighters / teams;
+        int extra_fighters = fighters % teams;
+        int result = (medium_in_team * (teams - extra_fighters) * (fighters - medium_in_team) +
+                      extra_fighters * (medium_in_team + 1) * (fighters - medium_in_team - 1)) / 2;
+
+        std::cout << result << std::endl;
+    }
+}
