1a8257c72ef20d21271a1c1c77dcfec30a7fda05
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-02-10 17:12:13 +0300

    1494 passed & report

--- N1494.cpp
+++ N1494.cpp
@@ -6,29 +6,35 @@ int main() {
     int n;
     std::cin >> n;
     // special data structure stack (first in - last out)
+    // helps to check order of balls
     std::stack<int> stack_of_balls;
-    bool result = true;
+    // true if not cheater
+    bool cheater = false;
+    // the biggest number of ball was input
     int temp = 0;
     for (int i = 0; i < n; ++i) {
         int cur_ball;
         std::cin >> cur_ball;
+        // check and update stack of the balls
         if (temp < cur_ball) {
             for (int j = temp + 1; j < cur_ball; ++j) {
                 stack_of_balls.push(j);
             }
             temp = cur_ball;
         } else {
+            // check if current ball is on the top of stack
             if (cur_ball == stack_of_balls.top()) {
+                // update stack by pop the highest
                 stack_of_balls.pop();
             } else {
-                result = false;
+                cheater = true;
             }
         }
-
     }
-    if (result) {
-        std::cout << "Not a proof" << std::endl;
-    } else {
+
+    if (cheater) {
         std::cout << "Cheater" << std::endl;
+    } else {
+        std::cout << "Not a proof" << std::endl;
     }
 }
