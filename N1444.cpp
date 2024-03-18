0bb0f12e41cdb37c0d657455e428cd79f502008a
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-03-18 16:11:26 +0300

    finish?

--- N1444.cpp
+++ N1444.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
 
 #define PI 3.14159265358979323846
 #define EPS 1e-10
@@ -9,16 +10,102 @@
 struct point_struct {
     int x;
     int y;
-    double len;
     double angle;
-    int num;
+    int id;
+    double len;
 };
 
-point_struct data[30000];
+const int MAX_N = 30001;
+point_struct data_points[MAX_N];
 
 bool compare(point_struct a, point_struct b) {
     if (abs(a.angle - b.angle) > EPS) {
         return a.angle < b.angle;
     }
-    return a.len < b.len
+
+    return a.len < b.len;
+}
+
+void quick_sort(int left, int right) {
+    int i = left;
+    int j = right;
+
+    point_struct x = data_points[(left + right) / 2];
+
+    while (i <= j) {
+        while (compare(data_points[i], x)) {
+            i++;
+        }
+        while (compare(x, data_points[j])) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(data_points[i], data_points[j]);
+
+            i++;
+            j--;
+        }
+    }
+
+    if (i < right) {
+        quick_sort(i, right);
+    }
+    if (left < j) {
+        quick_sort(left, j);
+    }
+}
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> data_points[i].x >> data_points[i].y;
+        data_points[i].id = i;
+
+        if (i == 0) {
+            data_points[i].angle = INT_MIN;
+            data_points[i].len = 0;
+        } else if (data_points[i].x == data_points[0].x){
+            if (data_points[i].y  > data_points[0].y) {
+                data_points[i].angle = 90;
+            } else {
+                data_points[i].angle = -90;
+            }
+            data_points[i].len = abs(data_points[i].y - data_points[0].y);
+        } else {
+            data_points[i].angle = atan((double) (data_points[i].y - data_points[0].y) / (data_points[i].x - data_points[0].x)) * 180.0 / PI;
+            if (data_points[i].x <= data_points[0].x) {
+                data_points[i].angle -= 180;
+            }
+            data_points[i].len = abs(sqrt((double) (pow((data_points[i].x - data_points[0].x), 2) + pow((data_points[i].y - data_points[0].y), 2))));
+        }
+    }
+
+    quick_sort(0, n - 1);
+
+    double max_a = 360 + data_points[1].angle - data_points[n - 1].angle;
+    int counter = 1;
+
+    for (int i = 1; i < n - 1; i++) {
+        if (data_points[i + 1].angle - data_points[i].angle > max_a) {
+            max_a = data_points[i + 1].angle - data_points[i].angle;
+            counter = i + 1;
+        }
+    }
+
+    cout << n << endl;
+    cout << 1 << endl;
+
+    for (int i = counter; i < n; i++) {
+        cout << data_points[i].id + 1 << endl;
+    }
+
+    for (int i = 1; i < counter; i++) {
+        cout << data_points[i].id + 1 << endl;
+    }
+
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,92 +1,111 @@
 #include <iostream>
 #include <cmath>
 
-#define PI 3.14159265358979323846
+using namespace std;
 
-// new struct for points, it keeps x, y, number of the point and angle
+#define PI 3.14159265358979323846
+#define EPS 1e-10
+
+// new struct for points, it keeps x, y, number of the point, length and angle
 struct point_struct {
     int x;
     int y;
     double angle;
-    int num;
+    int id;
+    double len;
 };
 
-point_struct data[10000];
+const int MAX_N = 30001;
+point_struct data_points[MAX_N];
 
 bool compare(point_struct a, point_struct b) {
-    return a.angle < b.angle;
+    if (abs(a.angle - b.angle) > EPS) {
+        return a.angle < b.angle;
+    }
+
+    return a.len < b.len;
 }
 
-void quick_sort(int left, int right){
+void sort(int left, int right) {
     int i = left;
     int j = right;
 
-    point_struct median = data[(left + right) / 2];
+    point_struct x = data_points[(left + right) / 2];
 
     while (i <= j) {
-        while (compare(data[i], median)) {
-            ++i;
+        while (compare(data_points[i], x)) {
+            i++;
         }
-        while (compare(median, data[j])) {
-            --j;
+        while (compare(x, data_points[j])) {
+            j--;
         }
+
         if (i <= j) {
-            std::swap(data[i], data[j]);
-            ++i;
-            --j;
+            swap(data_points[i], data_points[j]);
+
+            i++;
+            j--;
         }
     }
+
     if (i < right) {
-        quick_sort(i, right);
+        sort(i, right);
     }
     if (left < j) {
-        quick_sort(left, j);
+        sort(left, j);
     }
-};
+}
 
 int main() {
     int n;
-    std::cin >> n;
 
-    int first_x = 1000000;
-    int first_id = 0;
+    cin >> n;
 
     for (int i = 0; i < n; ++i) {
-        int p_x;
-        int p_y;
+        std::cin >> data_points[i].x >> data_points[i].y;
+        data_points[i].id = i;
 
-        std::cin >> p_x >> p_y;
-
-        if (p_x < first_x) {
-            first_id = i;
-            first_x = p_x;
-        }
-
-        data[i].x = p_x;
-        data[i].y = p_y;
-        data[i].num = i;
-    }
-
-    // here we analyze the angles
-    for (int i = 0; i < n; ++i) {
-        if (data[i].num == first_id) {
-            data[i].angle = -360;
-        } else {
-            if (data[i].x == data[first_id].x) {
-                if (data[i].y > data[first_id].y) {
-                    data[i].angle = 90;
-                } else {
-                    data[i].angle = -90;
-                }
+        if (i == 0) {
+            data_points[i].angle = INT_MIN;
+            data_points[i].len = 0;
+        } else if (data_points[i].x == data_points[0].x){
+            if (data_points[i].y  > data_points[0].y) {
+                data_points[i].angle = 90;
             } else {
-                data[i].angle = atan((double) (data[i].y - data[first_id].y) / (data[i].x - data[first_id].x)) * 180.0 / PI;
+                data_points[i].angle = -90;
             }
+            data_points[i].len = abs(data_points[i].y - data_points[0].y);
+        } else {
+            data_points[i].angle = atan((double) (data_points[i].y - data_points[0].y) / (data_points[i].x - data_points[0].x)) * 180.0 / PI;
+            if (data_points[i].x <= data_points[0].x) {
+                data_points[i].angle -= 180;
+            }
+            data_points[i].len = abs(sqrt((double) (pow((data_points[i].x - data_points[0].x), 2) + pow((data_points[i].y - data_points[0].y), 2))));
         }
     }
 
-    quick_sort(0, n - 1);
+    sort(0, n - 1);
 
-    std::cout << first_id + 1 << " " << data[n / 2].num + 1 << std::endl;
+    double max_a = 360 + data_points[1].angle - data_points[n - 1].angle;
+    int counter = 1;
+
+    for (int i = 1; i < n - 1; i++) {
+        if (data_points[i + 1].angle - data_points[i].angle > max_a) {
+            max_a = data_points[i + 1].angle - data_points[i].angle;
+            counter = i + 1;
+        }
+    }
+
+    cout << n << endl;
+    cout << 1 << endl;
+
+    for (int i = counter; i < n; i++) {
+        cout << data_points[i].id + 1 << endl;
+    }
+
+    for (int i = 1; i < counter; i++) {
+        cout << data_points[i].id + 1 << endl;
+    }
 
     return 0;
 }
